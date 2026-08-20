a6abb1f9a4a80dae5cde4a92411c0ddac94fafbe
JuanVilla <juancruzvilla030@gmail.com>
2026-08-19 22:56:55 -0300

    UserController y UserService completos

--- WebApi/Controllers/UserController.cs
+++ WebApi/Controllers/UserController.cs
@@ -1,16 +1,75 @@
-using System.Net.Http.Headers;
+using Domain.Entities;
 using Application.Services;
+using Microsoft.AspNetCore.Mvc;
 
 
-namespace Application.Services;
+namespace WebApi.Controllers;
 
-public class UserController
+[ApiController]
+[Route("api/[controller]")]
+
+public class UserController : ControllerBase
 {
     private readonly UserService _userService;
     public UserController(UserService userService)
     {
         _userService = userService;
     }
+
+
+    [HttpGet]
+    public async Task<IActionResult>GetAll()
+    {
+        return Ok(await _userService.GetAll());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult>GetById(Guid Id)
+    {
+     var user =await _userService.GetById(Id);
+        if (user != null){
+            return Ok (user);
+        }
+        else
+        {
+            return NotFound();
+            
+        }
+    }
+
+    [HttpGet("email/{email}")]
+    public async Task<IActionResult>GetByEmail(string email)
+    {
+        var user = await _userService.GetByEmail(email);
+        if (user != null)
+        {
+            return Ok(user);
+        }
+        else
+        {
+            return NotFound();
+        }
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddUser([FromBody]User user)
+    {
+        var created = await _userService.Add(user);
+        return Ok(created);
+        
+    }
+
+    [HttpPut("{id}")]
+    
+    public async Task<IActionResult>UpdateUser(Guid id, [FromBody] User user)
+    {
+       await _userService.Update(user);
+       return NoContent();
+        
+    }
+
+
+
 
 
 
